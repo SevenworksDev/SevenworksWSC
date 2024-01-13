67d8a4404c57b2e9fd40952a92cdbb7dc9db8259
Sevenworks <91027492+SevenworksDev@users.noreply.github.com>
2024-01-12 20:21:36 -0600

    Create anywhere_patcher.py

---
+++ Anywhere Patcher/anywhere_patcher.py
@@ -0,0 +1,84 @@
+import subprocess, shutil, os, time
+
+print("""
+                                  ››—›››                    
+                     ››››       ›z666666ü›                  
+                  ›ÏÏüüüüÏz{   ›Ï6{›  ›ü6ü›                 
+                 ›ÏüÏ›››—züüz› í6z›     66{                 
+                 {ÏÏ›     —Ïüí›z6í›     ü6í                 
+                 {ÏÏ   {{{{{üÏ{{ííízzzzí66z››               
+          {ÏÏÏzzízÏÏ›zÏÏzz{—üÏ{{zÏÏÏÏzÏÏzzíííÏÏüz—          
+          {ÏüüüüüüüüÏzíííí{—üÏízÏzzíízíízÏü66üzí6z          
+          {ÏüÏÏüÏüÏüüüÏüüüüüüÏz{ííÏüüüüüüzííü6üzÏz›         
+          {ÏüüüÏüüÏüÏüüÏüüÏüüüüüüÏz{{íÏüüüüÏí6üÏzz›         
+          {ÏüÏüüÏüüÏüÏüüÏüüÏÏüÏüüüüüüÏ—üüüüü{66Ïzz›         
+          {ÏüüÏüüüüüüÏüüÏüüüüÏüÏÏüÏÏüü—ÏüüüÏ—66ÏíÏ›         
+          {ÏüÏüüÏzÏüÏüüÏüüÏüüüüüüÏüüüü—ÏüüüÏ—ü6ÏíÏ›         
+          {ÏüüÏüü—íÏ {üÏÏüüüüÏüÏüüÏüÏü—Ïüüüü—ü6ÏíÏ›         
+          {ÏÏüüüüz—í››Ï——z—Ï{íüüÏüüÏüü—ÏüüüÏ—ü6Ïíz›         
+          —ÏüÏüÏüÏ››Ï—í›Ïz{ÏÏÏüüüÏüüüü{Ïüüüü—ü6Ïíz›         
+          —züÏüüüüz{üz›—üz—Ï{{ÏüüÏüÏüÏ{ÏüüüÏ—ü6Ïíz›         
+          ›züüÏüÏüüüüÏzÏÏz—Ï{{üÏüüÏüüü{züüüÏ—ü6Ïíz›         
+          ›zÏüüÏüÏÏüÏüüüüÏÏÏzíüüÏüüÏüü{züüüü—ü6Ïzz          
+          ›íüüüÏüüüÏüüÏÏüüüüüüüüÏüüÏüÏ{ÏüüüÏ—üüÏ——          
+           —zÏüüüÏüüÏüüüÏüÏüÏüÏüüÏüüüü{ÏüüüÏ—6üí            
+             ›—zzÏüüüÏüüÏüüÏüüÏüüÏüÏüü—ÏüüüÏ—üÏ—            
+                  —íÏÏüüüÏüüÏüüÏüüÏüüü—ÏüüüÏ                
+                      ›{ÏÏÏüüüÏüÏüüÏüü—züüüz                
+                          ›—íÏüüüüüüüü—   ›                 
+                              ››—íÏüüü›                     
+                                   ›››                      
+                                               ''Bring me to Life''
+""")
+
+time.sleep(1.5)
+os.system("cls")
+
+print("""
+    ÆÆÆ      ÆÆ   ÆÆ ÆÆ    ÆÆ  ÆÆ   ÆÆ   Æ  ÆÆ   ÆÆ  ÆÆÆÆÆÆ    ÆÆÆÆ    ÆÆÆÆÆÆ 
+   ÆÆ ÆÆ    ÆÆÆÆ ÆÆÆ ÆÆÆ  ÆÆÆ  ÆÆÆ  ÆÆ  ÆÆ  ÆÆÆ  ÆÆ  ÆÆÆ     ÆÆÆ  ÆÆÆ  ÆÆÆ
+   ÆÆ ÆÆ    ÆÆÆÆÆÆÆÆ  ÆÆÆÆÆÆ    ÆÆ  ÆÆ  ÆÆ  ÆÆÆÆÆÆÆ  ÆÆÆÆÆÆ  ÆÆ  ÆÆÆ   ÆÆÆÆÆ 
+  ÆÆÆÆÆÆÆ   ÆÆÆ ÆÆÆÆ    ÆÆÆ     ÆÆ  ÆÆ  ÆÆ  ÆÆÆ ÆÆÆ  ÆÆÆ     ÆÆÆÆÆÆÆ   ÆÆ    
+ ÆÆÆ   ÆÆÆ  ÆÆÆ  ÆÆÆ    ÆÆÆ      ÆÆÆ ÆÆÆ    ÆÆÆ  ÆÆ  ÆÆÆÆÆÆ  ÆÆÆÆ  ÆÆÆ ÆÆÆÆÆÆ
+
+         ÆÆÆÆÆÆÆ   ÆÆÆ  ÆÆÆÆÆÆÆÆ ÆÆÆÆÆÆÆ  ÆÆÆ  ÆÆÆ  ÆÆÆÆÆÆ ÆÆÆÆÆÆÆÆ       
+         ÆÆ   ÆÆ  ÆÆ ÆÆ    ÆÆÆ   ÆÆÆ      ÆÆÆÆÆÆÆÆ  ÆÆÆÆ   ÆÆ   ÆÆ        
+         ÆÆÆÆÆÆ  ÆÆ   ÆÆ   ÆÆÆ   ÆÆÆ      ÆÆÆ  ÆÆÆ  ÆÆÆ    ÆÆÆÆÆÆÆ        
+         ÆÆÆ    ÆÆÆÆÆÆÆÆÆ  ÆÆÆ   ÆÆÆÆÆÆÆ  ÆÆÆ  ÆÆÆ  ÆÆÆÆÆÆ ÆÆÆ ÆÆÆÆ       
+""")
+
+print("Made by Sevenworks   |   https://github.com/SevenworksDev/SevenworksWSC")
+
+print("[0.00%]       Checking if wszst exists...")
+
+if not shutil.which("wszst"):
+    print("Error: wszst either not installed or added to PATH.")
+    print("Download and setup wszst from https://szs.wiimm.de/wszst and try again.")
+    input()
+    exit()
+
+print("[14.28%]      Ready to patch 0000005f.app...")
+
+print("[28.54%]      Extracting 0000005f.app...")
+subprocess.run(["wszst", "-q", "EXTRACT", "0000005f.app"])
+
+print("[42.83%]      Patching 0000005f.app Opera Filter...")
+with open("0000005f.d\\arc\\opera\\myfilter.ini", "w") as f:
+    f.write("[prefs]\nprioritize excludelist=0\n\n[include]\nfile:/cnt/*\nhttps://*.shop.wii.com/*\nhttps://*\nmiip:*\nhttp://*\nhttps://*\n\n[exclude]\n*")
+
+print("[57.12%]      Building 0000005f.u8...")
+subprocess.run(["wszst", "-q", "C", "0000005f.d"])
+
+print("[71.39%]      Making a backup of 0000005f.app...")
+shutil.copy("0000005f.app", "0000005f.app.bak")
+
+print("[85.67%]      Renaming 0000005f.u8 to 0000005f.app...")
+shutil.move("0000005f.u8", "0000005f.app")
+
+print("[99.96%]      Removing temporary directory 0000005f.d...")
+shutil.rmtree("0000005f.d")
+
+input("[100%]        Done patching the Wii Shop Channel 0000005f.app file! It's now safe to close this window.")
+
+while True:
+    input()
